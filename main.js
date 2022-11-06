dee9443b2217935f2aee806e611805704c50d087
Dufelix55 <del.dufelix@gmail.com>
2022-11-06 13:20:25 -0300

    :tada: initial commit

--- main.js
+++ main.js
@@ -46,11 +46,35 @@ document.querySelector("#cards").innerHTML =
   /* /Aula 4 = voltar o main para o html, deixar só conteúdo das bandeiras\
   <main id="cards"></main>                                   
   */
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) +
   createCard(
-    "28/11",
+    "21/11",
     "segunda",
-    createGame("switzerland", "13:00", "brazil") +
-      createGame("portugal", "16:00", "uruguai")
+    createGame("england", "10:00", "iran") +
+      createGame("senegal", "13:00", "netherlands") +
+      createGame("united states", "16:00", "wales")
   ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard(
+    "22/11",
+    "terça",
+    createGame("argentina", "07:00", "saudi arabia") +
+      createGame("denmark", "10:00", "tunisia") +
+      createGame("mexico", "13:00", "poland") +
+      createGame("france", "16:00", "australia")
+  ) +
+  createCard(
+    "23/11",
+    "quarta",
+    createGame("morocco", "07:00", "croatia") +
+      createGame("germany", "10:00", "japan") +
+      createGame("spain", "13:00", "costa rica") +
+      createGame("belgium", "16:00", "canada")
+  ) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguai", "10:00", "south korea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  )
